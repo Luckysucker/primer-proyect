45ba0b3f3bc98f817d656464846908d48e2f8833
vigoss <laputagoready@gmail.com>
2024-06-26 12:29:55 -0400

    1ra Commit

---
+++ primerProyecto.cpp
@@ -0,0 +1,52 @@
+//#include <iostream>
+//
+//int main() {
+//	//int entero = 22;
+//	//int entero2 = 2222;
+//	//float flotante = 2.44;
+//	//double doble = 2.43444;
+//	//char caracter = 'c';
+//	//char cadena[10] = "aden quer";
+//	//printf("%d  %d  %.2f   %.3f  %c  %s",entero,entero2,flotante,doble, caracter,cadena);
+//	// para comentar shorcut  CTRL+K+C
+//	//int numero;	
+//	//scanf_s("%d", &numero); //lee la entrada de teclado en la consola
+//	//printf("El numero ingresado es: %d",numero);
+//
+//	//int num1, num2, suma, resta, multiplicacion, division;
+//	//printf("Ingresa el primer numero: ");
+//	//scanf_s("%d", &num1);
+//	//printf("Ingresa el primer numero: ");
+//	//scanf_s("%d", &num2);
+//
+//	//printf("Para salto de linea \n");
+//	//printf("Para salto de linea 2 \n");
+//
+//	/*int x=0, y, resultado;
+//
+//	1 + 1+x;*/
+//	//printf("Hola miundo");
+//
+//	
+//
+//}
+
+/*
+#include <iostream>
+using namespace std;
+int main() {
+	cout << sizeof(int)<<endl;
+	cout << sizeof(double) << endl;
+	cout << sizeof(float) << endl;
+	cout << sizeof(char) << endl;
+	cout << sizeof(char[2]) << endl;
+	cout << sizeof(string);
+}*/
+
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hola que tal amigo";
+	return 0;
+}
